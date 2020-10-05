1a733dafd7608430feaa2a183567e3bc7ff9ad61
Zhiyuan Jiang <Jerry31415926535@gmail.com>
2020-10-05 19:08:03 +1000

    Create env_install.sh

---
+++ Backend/env_install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install apache2
+sudo apt-get install php
+
+# Search mod
+apt-cache search libapache2-mod-php
+sudo apt-get install libapache2-mod-php
+
+sudo apt-get install mysql-server mysql-client
+sudo apt-get install php-mysql
+sudo apt-get install php-gd php-mbstring php-xml
